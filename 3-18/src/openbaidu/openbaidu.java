3dd89c777316f3ef55d738733758bb3d9f3ae9eb
qifeiluo <1971331903@qq.com>
2019-03-18 10:50:47 +0800

    打开百度

--- 3-18/src/openbaidu/openbaidu.java
+++ 3-18/src/openbaidu/openbaidu.java
@@ -3,6 +3,7 @@ package openbaidu;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class openbaidu {
